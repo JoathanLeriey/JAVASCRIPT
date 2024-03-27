8914ed5b20779ce9c2d1a6e12ba5498753f56e3f
Joathan Leriey da Paz Souza <131202050+JoathanLeriey@users.noreply.github.com>
2024-03-27 14:44:45 -0300

    funções

---
+++ exercicios/aula16/função003.js
@@ -0,0 +1,4 @@
+let v = function(x){ // basicamente vc pode fazer funções dentro de uma variavel
+    return x*2
+}
+console.log(v(5))
---
+++ exercicios/aula16/função02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){ //esse =0 é pra caso n tive no parametro ele ja considera como 0
+    return n1 + n2
+}
+
+console.log(soma(2, 5)) //nesse é dois parametros que o primeito vai fica no n1 e o segundo vai fica no n2
