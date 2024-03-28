6b4c15cdd072c777642fb1b74c51d6c63dc5c71f
Joathan Leriey da Paz Souza <131202050+JoathanLeriey@users.noreply.github.com>
2024-03-28 18:16:42 -0300

    ex018

---
+++ exercicios/aula16ex/ex018/script.js
@@ -0,0 +1,29 @@
+let num = document.querySelector('input#fnum') //para puxa os dados
+let lista = document.querySelector('select#flista') //para puxa os dados
+let res = document.querySelector('div#res') //para puxa os dados
+let valores = [] //para coloca todos os valores que for adicionado
+
+function isNumero(n) { // esse é para verifica se vc colocou um nemero ou n 
+    if(Number(n) >= 1 && Number(n) <= 200) { 
+        return true 
+    }else {
+        return false
+    }
+    
+}
+
+function inLista(n, l) { // para sabe se o numero adicionado ja esta na lista ou n
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    }else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        
+    }else {
+        window.alert('Valor invalido ou ja encontrado na lista.')
+    }
+}
